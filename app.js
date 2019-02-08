104096f71d92b73922f4b2b082e5f402b2ccbe1b
FrankMuller <dmitrysvetlov113@gmail.com>
2019-02-08 15:57:15 +0200

    fixed bugs

--- app.js
+++ app.js
@@ -1,39 +1,43 @@
-
-
 class Character {
-  constructor(name, race, health, attack, attackSpeed) {
+  constructor(name, race, speed, health, attack, attackSpeed) {
     this.race = race;
     this.name = name;
-    this.health = health;
-    this.attack = attack;
-    this.attackSpeed = attackSpeed;
+
+    this.characteristic = {
+      speed: speed,
+      health: health,
+      attack: attack,
+      attackSpeed: attackSpeed
+    }
+    
     this.buffs = []
   }
 
   welcomeMessage() {
-    console.log(`welcome ${this.race} - ${this.name}, your health - ${this.health}, your attack ${this.attack} and your attackSpeed ${this.attackSpeed}, your buffs - ${this.buffs}`);
-  }
-
-  applyBuff(buff) {
-    this.attack += buff.attack;
-    this.attackSpeed += buff.attackSpeed;
-    this.buffs.push(buff.name);
-    this.welcomeMessage();
+    console.log(`
+      welcome ${this.race} - ${this.name},
+      speed: ${this.characteristic.speed},
+      health: ${this.characteristic.health},
+      attack: ${this.characteristic.attack},
+      attackSpeed: ${this.characteristic.attackSpeed},
+      buffs: ${this.buffs}`);
   }
 }
 
 class Orc extends Character {
   constructor(name, message) {
-    super(name, 'Orc', 2000, 500, 15);
+    super(name, 'Orc', 200, 2000, 500, 15);
     super.welcomeMessage();
     this.message = message;
     this.say();
   }
 
   say() {
-    console.log(`${this.message} I\'m ${this.race} race - ${this.race}, your health - ${this.health}, your attack ${this.attack} and your attackSpeed ${this.attackSpeed}`);
+    console.log(`${this.message} I\'m ${this.race}`);
   }
 }
 
 const orc = new Orc('Bonobo', 'Loc tarogar.... daboo....');
-orc.applyBuff(berserker);
+
+berserker.applyBuff(orc);
+windWalk.applyBuff(orc);
--- buffs.js
+++ buffs.js
@@ -1,9 +1,39 @@
-class Berserker {
+class Buff {
+  constructor(name, duration) {
+    this.name = name;
+    this.duration = duration;
+  }
+
+  applyBuff(character) {
+    character.buffs.push(this.name);
+    for (let key in this.characteristic) {
+      character.characteristic[key] += this.characteristic[key];
+    }
+    setTimeout(() => {
+      character.welcomeMessage()
+    }, 200)
+  }
+}
+
+class Berserker extends Buff {
   constructor() {
-    this.name = 'berserker',
-    this.attack = 100;
-    this.attackSpeed = 3
+    super('berserker', 20*60);
+    this.characteristic = {
+      speed: 25,
+      attackSpeed: 5
+    }
+    
+  }
+}
+
+class WindWalk extends Buff {
+  constructor() {
+    super('wind walk', 20*60);
+    this.characteristic = {
+      speed: 50
+    }
   }
 }
 
 const berserker = new Berserker();
+const windWalk = new WindWalk();
